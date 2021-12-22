c3e8a7bfa694f3ea8b437116219a1fb746ca7e15
Adannaio <udohmercy911@gmail.com>
2021-12-22 10:03:45 +0000

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,62 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="home">
+    <h1>Github Typeahead</h1>
+
+    <div class="search-and-results">
+      <input type="text" placeholder="search for a github user" v-model="searchTerm" @input="searchUsers(searchTerm)"/>
+      <ul v-for="match in matches" :key="match.id" >
+        <router-link class="router-link" :to="{name: 'User', 
+        params: {
+          name: match.login, 
+          image: match.avatar_url,
+          githubLink: match.html_url
+          }
+        } 
+        ">
+          <li v-if="searchTerm"><img :src="match.avatar_url" alt="profile"><p>{{match.login}}</p></li>
+        </router-link>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import {ref} from 'vue';
+import axios from 'axios';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+
+  setup() {
+    let matches = ref(null);
+    const searchTerm = ref('');
+    
+
+    const searchUsers = async (searchTerm) => {
+      try {
+        const response = await axios.get('https://api.github.com/users');
+        const results = response.data;
+        matches.value = results.filter((result) => {
+          const regex = new RegExp(`^${searchTerm}`, 'gi');
+          const matchedSearchItem =  result.login.match(regex);
+          return matchedSearchItem
+        });
+
+        if(searchTerm.length === 0){
+          matches.value = [];
+        };
+      } catch (error) {
+         alert('there is a problem check your network access', error)
+       
+      }
+    }
+
+    return{
+      searchTerm,
+      matches,
+      searchUsers
+    }
   }
+  
 }
 </script>
---
+++ src/views/User.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="user-container">
+      <img :src="image" alt="">
+       <div class="second-div">
+          <div class="intro">
+            <h4>{{name}}</h4>
+            <a :href="githubLink">{{githubLink}}</a>
+          </div>
+      </div> 
+  </section>
+</template>
+
+<script>
+export default {
+  name : 'User',
+  props:['name','image', 'githubLink']
+}
+</script>
+
